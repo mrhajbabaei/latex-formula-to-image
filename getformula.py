911a0b9889229369c8a900b2599ef3ceef133246
mohammad reza hajbabaei <hajbabaei.mr@gmail.com>
2021-01-23 14:38:25 +0330

    fixed border issues

--- getformula.py
+++ getformula.py
@@ -1,7 +1,9 @@
 import os
+import cv2
 import glob
 import argparse
 import subprocess
+import numpy as np
 from pdf2image import convert_from_path
 from PIL import Image
 
@@ -13,11 +15,25 @@ def main(args):
         os.makedirs(output_dir)
         
     # help: https://stackoverflow.com/questions/8085520/generating-pdf-latex-with-python-script
-    content = r'''\documentclass{standalone}
-    \usepackage{xcolor}
-    \definecolor{khaki}{rgb}{0.94, 0.9, 0.55}
+    content = r'''\documentclass[x11names]{report}
+    \usepackage{background}
+    \definecolor{lightkhaki}{rgb}{0.94, 0.9, 0.55}
+    \thispagestyle{empty}
+    \renewcommand\fbox{\fcolorbox{lightkhaki}{lightkhaki}}
+    \backgroundsetup{
+        scale=1,
+        angle=0,
+        opacity=1,
+        contents={\begin{tikzpicture}[remember picture,overlay]
+                \path [left color = lightkhaki,middle color = lightkhaki, right color = lightkhaki] (current page.south west)rectangle (current page.north east);
+        \end{tikzpicture}}
+    }
     \begin{document}
-    \colorbox{khaki}{$\displaystyle %(formula)s$}
+        \centering
+        \setlength{\fboxsep}{2em}
+        \fbox{
+            $\displaystyle %(formula)s$
+        }	
     \end{document}
     '''
     args = parser.parse_args()
@@ -42,9 +58,17 @@ def main(args):
     pages = convert_from_path(file_path, 500)
     image_files_number = len(glob.glob1(output_dir, '*.png'))
     output_image_file = os.path.join(output_dir, f'formula-{image_files_number}.png')
+
     for page in pages:
-        width, height = page.size
-        page.crop((5, 5, width-5, height-5)).save(output_image_file, 'PNG')
+
+        nonwhite_positions = [(x,y) for x in range(page.size[0]) for y in range(page.size[1]) if page.getdata()[x+y*page.size[0]] != (255,255,255)]
+        rect = (min([x for x,y in nonwhite_positions]), min([y for x,y in nonwhite_positions]), max([x for x,y in nonwhite_positions]), max([y for x,y in nonwhite_positions]))
+        page.crop(rect).save(output_image_file, 'PNG')
+
+
+
+        # width, height = page.size
+        # page.crop((5, 5, width-5, height-5)).save(output_image_file, 'PNG')
 
     os.remove(file_path)
 
